c94fa2d9f4ac5a91a3c1fc80117b21f1c3e4b015
Carlos Salinas <carlossalinasmorales1@gmail.com>
2026-03-27 20:57:31 -0300

    Script mejorado y carpeta output vaciada

--- scripts/pipeline.sh
+++ scripts/pipeline.sh
@@ -1,13 +1,29 @@
 #!usr/bin/env bash
 
-cd ..
-set -euo pipefail
+set -euo pipefail # Detiene el pipeline si algo falla
+
+#Aqui se establece las carpeta raiz para mejorar la automatizacion del script
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+PROJECT_ROOT="$(cd "$SCRIPT_DIR/.." && pwd)"
+
+#Carpetas de archivos especificos
+DATA_DIR="$PROJECT_ROOT/data/raw"
+OUTPUT_DIR="$PROJECT_ROOT/output"
+LOG_DIR="$PROJECT_ROOT/logs"
+
+#Archivos especificos
+CSV_FILE="$DATA_DIR/titanic.csv"
+REPORT_FILE="$OUTPUT_DIR/class_count.txt"
+ERROR_LOG="$LOG_DIR/error.log"
+
+#TOKEN
 TOKEN_CURSO="FAE_Usach"
 
 
-wget https://raw.githubusercontent.com/datasciencedojo/datasets/master/titanic.csv -p ~/data/raw/ 2>> logs/error.log
 
-echo "Reporte generado por: $USER el $(date) - Token: $TOKEN_CURSO" > output/class_count.txt
+wget -O "$CSV_FILE" https://raw.githubusercontent.com/datasciencedojo/datasets/master/titanic.csv 2>> "$ERROR_LOG"
 
-tail -n +2 data/raw/titanic.csv | cut -d',' -f3 | sort | uniq -c | sort -nr >> output/class_count.txt
+echo "Reporte generado por: $USER el $(date) - Token: $TOKEN_CURSO" > "$REPORT_FILE"
+
+tail -n +2 "$CSV_FILE" | cut -d',' -f3 | sort | uniq -c | sort -nr >> "$REPORT_FILE"
 
