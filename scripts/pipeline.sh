acef083e1c745dcb98cbf4b6f468326f3a5e06c4
Carlos Salinas <carlossalinasmorales1@gmail.com>
2026-03-29 13:36:42 -0300

    Script mejorado

--- scripts/pipeline.sh
+++ scripts/pipeline.sh
@@ -6,15 +6,19 @@ set -euo pipefail # Detiene el pipeline si algo falla
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 PROJECT_ROOT="$(cd "$SCRIPT_DIR/.." && pwd)"
 
+CSV_FILE_NAME="titanic.csv"
+OUTPUT_FILE_NAME="class_count.txt"
+ERROR_FILE_NAME=error.log
+
 #Carpetas de archivos especificos
 DATA_DIR="$PROJECT_ROOT/data/raw"
 OUTPUT_DIR="$PROJECT_ROOT/output"
 LOG_DIR="$PROJECT_ROOT/logs"
 
 #Archivos especificos
-CSV_FILE="$DATA_DIR/titanic.csv"
-REPORT_FILE="$OUTPUT_DIR/class_count.txt"
-ERROR_LOG="$LOG_DIR/error.log"
+CSV_FILE="$DATA_DIR/$CSV_FILE_NAME"
+OUTPUT_FILE="$OUTPUT_DIR/$OUTPUT_FILE_NAME"
+ERROR_LOG="$LOG_DIR/$ERROR_FILE_NAME"
 
 #TOKEN
 TOKEN_CURSO="FAE_Usach"
@@ -23,21 +27,29 @@ echo VARIABLES DE DIRECTORIOS CREADAS CORRECTAMENTE
 
 
 if wget -O "$CSV_FILE" https://raw.githubusercontent.com/datasciencedojo/datasets/master/titanic.csv 2> "$ERROR_LOG"; then
-  echo "[OK] $CSV_FILE CREADO CORRECTAMENTE"
+  echo "[OK] $CSV_FILE_NAME CREADO CORRECTAMENTE"
 else
-  echo "[ERROR] Algo fallo, revisar $ERROR_LOG"
+  echo "[ERROR] Algo fallo, al crear $CSV_FILE_NAME revisar $ERROR_FILE_NAME"
 fi
 
-if echo "Reporte generado por: $USER el $(date) - Token: $TOKEN_CURSO" > "$REPORT_FILE"; then
+if echo "Reporte generado por: $USER el $(date) - Token: $TOKEN_CURSO" > "$OUTPUT_FILE"; then
   echo "[OK] REPORT USER Y TOKEN CREADO CORRECTAMENTE"
 else
-  echo "[ERROR] Algo fallo, revisar creacion de inea de reporte"
+  echo "[ERROR] ALGO FALLO AL CREAR REPORT USER Y TOKEN EN $OUTPUT_FILE_NAME"
 fi
 
-if tail -n +2 "$CSV_FILE" | cut -d',' -f3 | sort | uniq -c | sort -nr >> "$REPORT_FILE"; then
-  echo "[OK] $CSV_FILE TRANSFORMADO CORRECTAMENTE"
+if tail -n +2 "$CSV_FILE" | cut -d',' -f3 | sort | uniq -c | sort -nr >> "$OUTPUT_FILE"; then
+  echo "[OK] $CSV_FILE_NAME TRANSFORMADO CORRECTAMENTE"
 else
-  echo "[ERROR] PROBLEMAS AL TRANSFORMAR $CSV_FILE"
+  echo "[ERROR] PROBLEMAS AL TRANSFORMAR $CSV_FILE_NAME"
 fi
 
-echo "[OK END]"
+echo "******* OUTPUT GENERADO *******"
+if cat $OUTPUT_FILE; then
+  echo "******************************"
+  echo "[OK] PIPELINE EJECUTADO CON EXITO"
+else
+  echo "[ERROR] FALLA AL LEER OUTPUT CREADO"
+fi
+
+exit 0
